cb000b3ad09b60544f5c69bc84d97853c5ce7ea9
Cesar Villao <cesarvillao@hotmail.com>
2022-05-21 01:09:39 -0400

    styled buttons responsiveness

--- client/src/Components/Button.js
+++ client/src/Components/Button.js
@@ -11,8 +11,20 @@ const Wrapper = styled.button`
   border-radius: 50px;
   border: none;
   width: 100%;
-  height: 25px;
+  height: 30px;
   margin-top: 20px;
+  border-bottom: 4px #82c8fa solid;
+  border-right: 2px #82c8fa solid;
+
+  &:hover {
+    margin: 19px 0 -1 0;
+  }
+
+  &:active {
+    padding: 0;
+
+    border: none;
+  }
   @media (min-width: ${breakpoints.xs}) {
   }
 `;
