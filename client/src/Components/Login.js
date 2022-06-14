3ab1341bde98b395d1a2c44bad0bce9bd7c9bf61
Cesar Villao <cesarvillao@hotmail.com>
2022-06-14 11:22:07 -0400

    test

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
   const handleSubmit = (ev) => {
     //handle loggin
     fetch("https://scheduler24-7.herokuapp.com/api/login", {
-      mode: "no-cors",
+      mode: "cors",
       method: "POST",
       body: JSON.stringify({
         email: userInfo.email.toLowerCase(),
